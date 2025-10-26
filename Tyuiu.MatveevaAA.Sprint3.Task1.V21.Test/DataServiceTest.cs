16c69d342e4c001e5fa8c814aaa6c00c37191ffe
Anastasia Matveeva <kekkekyshk@gmail.com>
2025-10-26 22:57:08 +0500

    Task1

---
+++ Tyuiu.MatveevaAA.Sprint3.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MatveevaAA.Sprint3.Task1.V21.Lib;
+
+namespace Tyuiu.MatveevaAA.Sprint3.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            var ds = new DataService();
+            double actual = ds.GetMultiplySeries(1, 1, 12);
+            double expected = 16947810.880588; // рассчитанное значение
+            Assert.AreEqual(expected, actual, 1e-6);
+        }
+    }
+}
---
+++ Tyuiu.MatveevaAA.Sprint3.Task1.V21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Tyuiu.MatveevaAA.Sprint3.Task1.V21.Lib;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Матвеева Анастасия Андреевна | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3 | Тема: Оператор цикла while                                  *");
+        Console.WriteLine("* Задание #1 | Вариант #21                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("p = Π_{k=1..12} ((0.5 + k/5) / (cos(1) + 0.5))^3");
+        Console.WriteLine("***************************************************************************");
+
+        double result = ds.GetMultiplySeries(1, 1, 12);   // x = 1 зашит в библиотеке
+        Console.WriteLine($"Результат" + result);
+        Console.ReadKey();
+    }
+}
